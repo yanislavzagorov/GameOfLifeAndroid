9a3c4afa44cc10bcd815e2a959c7686654ba67e3
yanislavzagorov <yanislavzagorov@yahoo.com>
2017-04-17 11:38:45 +0200

    Added functional byte[][] board and drawing of board

--- app/src/main/java/com/example/android/gameoflifeandroid/TextQrGol.java
+++ app/src/main/java/com/example/android/gameoflifeandroid/TextQrGol.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.constraint.solver.widgets.Rectangle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 
 public class TextQrGol extends AppCompatActivity {
@@ -17,20 +21,64 @@ public class TextQrGol extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_qr_gol);
     }
-
 }
 
 
 class GameViewer extends View {
-    Paint paint = new Paint();
+    Paint aliveCellPaint = new Paint();
+    Paint deadCellPaint = new Paint();
+    Rectangle aliveCell = new Rectangle();
+    Rectangle deadCell = new Rectangle();
+    public byte rows = 50;
+    public byte columns = 50;
+    byte[][] board = new byte[rows][columns];
+    public float topRect = 0;
+    public float bottomRect = 30;
+    public float leftRect = 0;
+    public float rightRect = 30;
+
 
     public GameViewer(Context context, AttributeSet attrs) {
         super(context, attrs);
-        paint.setColor(Color.BLACK);
+        aliveCellPaint.setColor(Color.BLACK);
+        deadCellPaint.setColor(Color.WHITE);
+        board[10][10]=1;
     }
 
     @Override
     public void onDraw(Canvas canvas) {
+        canvas.drawRect(leftRect,topRect,rightRect,bottomRect,aliveCellPaint);
 
+        for(int i = 0; i<rows; i++)
+        {
+            for(int j = 0; j<columns; j++)
+            {
+                if(board[i][j] == 0){
+                    canvas.drawRect(leftRect,topRect,rightRect,bottomRect,deadCellPaint);
+                }else{
+                    canvas.drawRect(leftRect,topRect,rightRect,bottomRect,aliveCellPaint);
+                }
+                leftRect += 30;
+                rightRect += 30;
+            }
+            leftRect = 0;
+            rightRect = 30;
+            topRect += 30;
+            bottomRect += 30;
+        }
     }
 }
+
+/*
+class GameOfLifeBoard {
+    byte rows;
+    byte columns;
+
+    public GameOfLifeBoard(byte rows, byte columns){
+        this.rows=rows;
+        this.columns=columns;
+        byte[][] board = new byte[rows][columns];
+    }
+}
+*/
+
